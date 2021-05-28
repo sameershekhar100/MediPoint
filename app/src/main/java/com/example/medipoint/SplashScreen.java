0fac9d4172087b143285cf27fb943e23dd78efdb
MERCUROUS <64265450+MERCUROUS@users.noreply.github.com>
2021-05-28 18:34:58 +0530

    added

---
+++ app/src/main/java/com/example/medipoint/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.medipoint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        int SPLASH_DISPLAY_LENGTH = 1000;
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, 1000);
+
+    }
+}
